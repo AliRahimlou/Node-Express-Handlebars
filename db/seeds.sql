6862e30302c84601d10467cd7f473ba761a1b1b4
Ali Rahimlou <Rahimlou91@gmail.com>
2019-12-07 12:31:26 -0500

    it finally works

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Seed Burgers into Database 
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
+  ("Whooper", false, "2016-11-05 04:20:01"),
+  ("Quarter Pounder", false, "2016-11-05 16:20:01");
