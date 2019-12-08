15919b78fe5be159359cf879029b1c7eb08b30b1
Ali Rahimlou <Rahimlou91@gmail.com>
2019-12-07 22:47:22 -0500

    hero

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
-var mysql = require("mysql");
+// Node Dependency
+var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "docker",
-  database: "burgers_db"
-});
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Iforget123', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+}
 
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+// Export the Connection
 module.exports = connection;
