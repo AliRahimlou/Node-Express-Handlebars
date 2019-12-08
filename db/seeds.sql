03a46c368dc843e667badb7fc7a3270798d0b44b
Ali Rahimlou <Rahimlou91@gmail.com>
2019-12-07 22:43:01 -0500

    hero

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
--- Seed Burgers into Database 
-INSERT INTO burgers(burger_name, devoured, date)
-VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
-  ("Whooper", false, "2016-11-05 04:20:01"),
-  ("Quarter Pounder", false, "2016-11-05 16:20:01");
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
